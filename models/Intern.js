732d6d779b44349b8c62fbc2c4b84e0427bf15c5
Samantha Jalowicz <sjalowicz@wireless-10-104-40-216.umd.edu>
2019-05-08 17:54:39 -0400

    models folder

---
+++ models/Intern.js
@@ -0,0 +1,68 @@
+var mongoose = require('mongoose');
+
+var internSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    company: {
+        type: String,
+        required: true
+    },
+    age: {
+      type: Number,
+      min: 0,
+      max: 1000,
+      required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    characteristicsInterests: [String]
+});
+
+var activitySchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    }
+});
+
+var foodSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        min: 0,
+        max: 5,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    reviews: [String]
+});
+
+var Intern = mongoose.model('Intern', internSchema);
+
+module.exports = Intern;
